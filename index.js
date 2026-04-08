385b49e7309ab53cf68514c00040251ee1a3f9bf
ema <emanuelcaceres02@hotmail.com.ar>
2026-04-07 23:50:28 -0300

    agregue comentarios al PORT

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const dotenv = require ('dotenv')
+const dotenv = require ('dotenv')// definimos una constante donde requerimos codigo del paquete dotenv
 dotenv.config()
 
-const http = require("http")//toda la libreria http esta almancenada en un objeto de nombre http
+const http = require("http")//toda la libreria http esta almancenada en un objeto de nombre http//
+
 //creo una variable
 let valor = 28;
 
 const name = require('./another') //con "./" requerimos archivos de la misma carpeta para utilizar su contenido
-console.log({name}); 
+console.log({name});//imprime el nombre martin
+
+
 function requestController(){
     //logica de nuestra funcion
-     console.log("recibimos una nueva request : D");
+     console.log("recibimos una nueva request :+D");
 
 }
 //configurar nuestro servidor 
 const server = http.createServer(requestController)// es un metodo crea un servidor y lo 
 // almacenamos en una constante server
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT //"PORT es una variable que suele existir en todos los entornos productivos"
 server.listen(PORT, function(){
     console.log('aplicacion corriendo en puerto ' +PORT)
 })//server escucha en el puerto 4000  //luego se le pasa una funcion 
